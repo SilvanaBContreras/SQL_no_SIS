f47065bfc44d40b258d937c94b0b5d3e3f11ab54
SilvanaBContreras <silvanacontreras76@gmail.com>
2026-09-03 12:49:30 -0300

    Corrige BDnoSQL_TP1: pasa de submódulo a carpeta con archivos

---
+++ BDnoSQL_TP1/tp1_contreras_monteros-domogalla_scaiano/tp1_ej03.py
@@ -0,0 +1,91 @@
+import csv
+
+# Ubicacion del archivo CSV con el contenido provisto por la catedra
+archivo_entrada = 'full_export.csv'
+#nombre_archivo_resultado_ejercicio = 'tpY_ejXX.txt'
+
+nombre_archivo_resultado_ejercicio = 'tp1_ej03.txt'
+
+# Objeto de configuracion para conectarse a la base de datos usada en este ejercicio
+conexion = []
+
+
+# Funcion que dada la configuracion y ubicacion del archivo, carga la base de datos, genera el reporte, y borra la
+# base de datos
+def ejecutar(file, conn):
+    db = inicializar(conn)
+    df_filas = csv.DictReader(open(file, "r", encoding="utf-8"))
+    for fila in df_filas:
+        procesar_fila(db, fila)
+    generar_reporte(db)
+    finalizar(db)
+
+
+# Funcion que dado un archivo abierto y una linea, imprime por consola y guarda al final de archivo esa linea
+def grabar_linea(archivo, linea):
+    print(linea)
+    archivo.write(linea+'\n')
+
+
+# Funcion para poner el codigo que cree las estructuras a usarse en el este ejercicio
+# Debe ser implementada por el alumno
+def inicializar(conn):
+    return []
+    # crear db
+
+
+# Funcion que dada una linea del archivo CSV (en forma de objeto) va a encargarse de insertar el (o los) objetos
+# necesarios
+# Debe ser implementada por el alumno
+#def procesar_fila(database, fila):
+#    pass
+    # insertar elemento en entidad para el ejercicio actual
+
+def procesar_fila(database, fila):
+    especialidad = (
+        fila["nombre_tipo_especialidad"],
+        fila["nombre_especialidad"]
+    )
+    if especialidad not in database:
+        database.append(especialidad)
+
+# Funcion que realiza el o los queries que resuelven el ejercicio, utilizando la base de datos.
+# Debe ser implementada por el alumno
+#def generar_reporte(database):
+
+    # archivo = open(nombre_archivo_resultado_ejercicio, 'w')
+    # luego para cada linea generada como reporte:
+    # grabar_linea(archivo, linea)
+#    pass
+
+def generar_reporte(database):
+    archivo = open(nombre_archivo_resultado_ejercicio, 'w')
+    grabar_linea(archivo, "Cantidad de especialidades asociadas a cada tipo:")
+    database.sort()
+    tipos = []
+    # tipos diferentes
+    for especialidad in database:
+        tipo = especialidad[0]
+        if tipo not in tipos:
+            tipos.append(tipo)
+
+    # cuántas especialidades tiene cada tipo
+    for tipo in tipos:
+        cantidad = 0
+        for especialidad in database:
+            if especialidad[0] == tipo:
+                cantidad = cantidad + 1
+        linea = tipo + "," + str(cantidad)
+        grabar_linea(archivo, linea)
+    archivo.close()
+
+
+# Funcion para el borrado de estructuras generadas para este ejercicio
+# Debe ser implementada por el alumno
+def finalizar(database):
+    pass
+    # Borrar la estructura de la base de datos
+
+
+# Llamado a la ejecucion del programa
+ejecutar(archivo_entrada, conexion)
---
+++ BDnoSQL_TP1/tp1_contreras_monteros-domogalla_scaiano/tp1_ej06.py
@@ -0,0 +1,103 @@
+import csv
+
+# Ubicacion del archivo CSV con el contenido provisto por la catedra
+archivo_entrada = 'full_export.csv'
+#nombre_archivo_resultado_ejercicio = 'tpY_ejXX.txt'
+
+nombre_archivo_resultado_ejercicio = 'tp1_ej06.txt'
+
+# Objeto de configuracion para conectarse a la base de datos usada en este ejercicio
+conexion = []
+
+
+# Funcion que dada la configuracion y ubicacion del archivo, carga la base de datos, genera el reporte, y borra la
+# base de datos
+def ejecutar(file, conn):
+    db = inicializar(conn)
+    df_filas = csv.DictReader(open(file, "r", encoding="utf-8"))
+    for fila in df_filas:
+        procesar_fila(db, fila)
+    generar_reporte(db)
+    finalizar(db)
+
+
+# Funcion que dado un archivo abierto y una linea, imprime por consola y guarda al final de archivo esa linea
+def grabar_linea(archivo, linea):
+    print(linea)
+    archivo.write(linea+'\n')
+
+
+# Funcion para poner el codigo que cree las estructuras a usarse en el este ejercicio
+# Debe ser implementada por el alumno
+def inicializar(conn):
+    return []
+    # crear db
+
+
+# Funcion que dada una linea del archivo CSV (en forma de objeto) va a encargarse de insertar el (o los) objetos
+# necesarios
+# Debe ser implementada por el alumno
+#def procesar_fila(database, fila):
+#    pass
+    # insertar elemento en entidad para el ejercicio actual
+
+def procesar_fila(database, fila):
+    if fila["nombre_especialidad"] == "carrera 100 m":
+        resultado = (
+            fila["nombre_torneo"],
+            int(fila["marca"]),
+            fila["nombre_especialidad"],
+            fila["nombre_deportista"]
+        )
+        database.append(resultado)
+
+# Funcion que realiza el o los queries que resuelven el ejercicio, utilizando la base de datos.
+# Debe ser implementada por el alumno
+#def generar_reporte(database):
+
+    # archivo = open(nombre_archivo_resultado_ejercicio, 'w')
+    # luego para cada linea generada como reporte:
+    # grabar_linea(archivo, linea)
+#    pass
+
+def generar_reporte(database):
+    archivo = open(nombre_archivo_resultado_ejercicio, 'w', encoding="utf-8")
+    grabar_linea(
+        archivo,
+        "Podio de carrera 100 m en cada torneo:"
+    )
+    grabar_linea(
+        archivo,
+        "Torneo,Especialidad,Deportista,Marca"
+    )
+    database.sort()
+    torneo_anterior = ""
+    posicion = 0
+    for resultado in database:
+        torneo = resultado[0]
+        if torneo != torneo_anterior:
+            torneo_anterior = torneo
+            posicion = 1
+        else:
+            posicion = posicion + 1
+
+        if posicion <= 3:
+            linea = (
+                resultado[0] + "," +
+                resultado[2] + "," +
+                resultado[3] + "," +
+                str(resultado[1])
+            )
+            grabar_linea(archivo, linea)
+    archivo.close()
+
+
+# Funcion para el borrado de estructuras generadas para este ejercicio
+# Debe ser implementada por el alumno
+def finalizar(database):
+    pass
+    # Borrar la estructura de la base de datos
+
+
+# Llamado a la ejecucion del programa
+ejecutar(archivo_entrada, conexion)
