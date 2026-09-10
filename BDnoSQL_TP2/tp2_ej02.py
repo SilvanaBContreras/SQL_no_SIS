9858bbc83566faa91e76e084b312da47f9393461
SilvanaBContreras <silvanacontreras76@gmail.com>
2026-09-09 23:21:58 -0300

    01 a 03 revisados. 04 renombrado anti conflicto

--- BDnoSQL_TP2/tp2_ej02.py
+++ BDnoSQL_TP2/tp2_ej02.py
@@ -1,18 +1,14 @@
 import csv
 import redis
 
-# Ubicacion del archivo CSV con el contenido provisto por la catedra
 archivo_entrada = 'full_export.csv'
 nombre_archivo_resultado_ejercicio = 'tp2_ej02.txt'
 
-# Objeto de configuracion para conectarse a la base de datos usada en este ejercicio
 conexion = {
     'redisurl': 'localhost',
     'redispuerto': 6379
 }
 
-# Funcion que dada la configuracion y ubicacion del archivo, carga la base de datos, genera el reporte, y borra la
-# base de datos
 def ejecutar(file, conn):
     import time
 
@@ -36,21 +32,13 @@ def ejecutar(file, conn):
     print(end - start)
 
 
-# Funcion que dado un archivo abierto y una linea, imprime por consola y guarda al final de archivo esa linea
 def grabar_linea(archivo, linea):
     print(linea)
     archivo.write(str(linea) + '\n')
 
-
 def inicializar(conn):
     r = redis.Redis(conn["redisurl"], conn["redispuerto"], db=0, decode_responses=True)
     return r
-    # crear db
-
-
-# Funcion que dada una linea del archivo CSV (en forma de objeto) va a encargarse de insertar el (o los) objetos
-# necesarios
-# Debe ser implementada por el alumno
 
 def procesar_fila(db, fila):
     id_deportista = fila["id_deportista"]
@@ -62,42 +50,42 @@ def procesar_fila(db, fila):
                                       "fecha_nacimiento": fecha_nacimiento,
                                       "nombre_pais_deportista": nombre_pais_deportista})
 
-# Funcion que realiza el o los queries que resuelven el ejercicio, utilizando la base de datos.
-# Debe ser implementada por el alumno
 
 def generar_reporte(db):
-    archivo = open(nombre_archivo_resultado_ejercicio, "w", encoding="utf-8")
+    archivo = open(
+        nombre_archivo_resultado_ejercicio,
+        "w",
+        encoding="utf-8"
+    )
 
-    # Titulo descriptivo y encabezado de columnas al inicio del reporte
     titulo_reporte = "Datos de los deportistas según el id solicitado"
-    encabezado_columnas = "id_deportista,nombre_deportista,fecha_nacimiento,nombre_pais_deportista"
+
+    encabezado_columnas = (
+        "id_deportista,nombre_deportista,"
+        "fecha_nacimiento,nombre_pais_deportista"
+    )
+
     grabar_linea(archivo, titulo_reporte)
     grabar_linea(archivo, encabezado_columnas)
 
-    claves = db.keys("*")
-    claves.sort(key=int) #para que devuelva ordenados
+    ids_solicitados = ["10", "20", "30"]
 
-    for clave in claves:
-        id_deportista = clave
+    for id_deportista in ids_solicitados:
+        ficha_deportista = db.hgetall(id_deportista)
 
-        if id_deportista in ["10", "20", "30"]:
-            ficha_deportista = db.hgetall(clave)
+        linea = (
+            id_deportista + ","
+            + ficha_deportista["nombre_deportista"] + ","
+            + ficha_deportista["fecha_nacimiento"] + ","
+            + ficha_deportista["nombre_pais_deportista"]
+        )
 
-            linea = (id_deportista + "," 
-            + ficha_deportista["nombre_deportista"] + "," 
-            + ficha_deportista["fecha_nacimiento"] + "," 
-            + ficha_deportista["nombre_pais_deportista"])
-
-            grabar_linea(archivo, linea)
+        grabar_linea(archivo, linea)
 
     archivo.close()
 
 
-# Funcion para el borrado de estructuras generadas para este ejercicio
 def finalizar(db):
     db.flushdb()
-    # Borrar la estructura de la base de datos
 
-
-# Llamado a la ejecucion del programa
 ejecutar(archivo_entrada, conexion)
